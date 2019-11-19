a616c36f4eedaa3e505d0b2103165fb0eed36b01
Josh <tarv360@gmail.com>
2019-11-19 22:24:19 +1030

    Added entry interface to mapped

--- src/mapped.rs
+++ src/mapped.rs
@@ -1,6 +1,70 @@
 use crate::*;
 use std::{borrow::Borrow, collections::hash_map::HashMap, hash::Hash};
 
+pub struct Occupied<'a, K: 'a, T: 'a> {
+    key: &'a K,
+    index: usize,
+    value: &'a mut T,
+}
+
+pub struct VacantEntry<'a, K: 'a, T: 'a>
+where
+    K: Hash + Clone + Eq,
+{
+    key: K,
+    storage: &'a mut MappedNovec<K, T>,
+}
+
+pub enum Entry<'a, K: 'a, T: 'a>
+where
+    K: Hash + Clone + Eq,
+{
+    Occupied(Occupied<'a, K, T>),
+    VacantEntry(VacantEntry<'a, K, T>),
+}
+
+impl<'a, K: 'a, T: 'a> Entry<'a, K, T>
+where
+    K: Hash + Clone + Eq,
+{
+    pub fn or_insert(self, default: T) -> (usize, &'a mut T) {
+        match self {
+            Entry::Occupied(Occupied { value, index, .. }) => (index, value),
+            Entry::VacantEntry(VacantEntry { key, storage }) => {
+                let index = storage.insert(key, default);
+                (index, storage.get_mut(index).unwrap())
+            }
+        }
+    }
+
+    pub fn or_insert_with<F: FnOnce() -> T>(self, default: F) -> (usize, &'a mut T) {
+        self.or_insert(default())
+    }
+
+    pub fn key(&self) -> &K {
+        match self {
+            Entry::Occupied(Occupied { key, .. }) => key,
+            Entry::VacantEntry(VacantEntry { key, .. }) => &key,
+        }
+    }
+
+    pub fn and_modify<F: FnOnce(&mut T)>(mut self, f: F) -> Self {
+        match &mut self {
+            Entry::Occupied(Occupied { value, .. }) => f(value),
+            _ => {}
+        }
+
+        self
+    }
+
+    pub fn or_default(self) -> (usize, &'a mut T)
+    where
+        T: Default,
+    {
+        self.or_insert(Default::default())
+    }
+}
+
 // Can be indexed by either a Key or a usize, usize will be faster
 // Keys stored in separate vector for faster iteration over values (probably doesnt matter)
 #[derive(Clone, Debug)]
@@ -8,7 +72,7 @@ pub struct MappedNovec<K, T>
 where
     K: Hash + Clone + Eq,
 {
-    pub map: HashMap<K, usize>,
+    map: HashMap<K, usize>,
     keys: Vec<Option<K>>,
     values: NoVec<T>,
 }
@@ -17,6 +81,17 @@ impl<K, T> MappedNovec<K, T>
 where
     K: Hash + Clone + Eq,
 {
+    pub fn entry<Q>(&mut self, key: K) -> Entry<K, T> {
+        match self.map.get(&key) {
+            Some(&index) => Entry::Occupied(Occupied {
+                key: self.keys[index].as_mut().unwrap(),
+                index,
+                value: self.values.get_mut(index).unwrap(),
+            }),
+            None => Entry::VacantEntry(VacantEntry { key, storage: self }),
+        }
+    }
+
     pub fn get(&self, index: usize) -> Option<&T> {
         self.values.get(index)
     }
@@ -43,7 +118,7 @@ where
         self.values.get_mut(*index)
     }
 
-    pub fn insert(&mut self, key: K, value: T) {
+    pub fn insert(&mut self, key: K, value: T) -> usize {
         let index = self.values.push(value);
 
         while self.keys.len() <= index {
@@ -52,6 +127,8 @@ where
 
         self.keys[index] = Some(key.clone());
         self.map.insert(key, index);
+
+        index
     }
 
     pub fn remove(&mut self, index: usize) -> Option<(K, T)> {
